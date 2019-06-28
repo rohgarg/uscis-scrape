de5cf1820d918c3083ceaabd788f49d0755dc891
Rohan Garg <rohgarg@ccs.neu.edu>
2019-06-27 19:25:22 -0700

    Add ability to save partial data on interrupt

--- scrape.py
+++ scrape.py
@@ -10,12 +10,15 @@ from commonutils import log_error, construct_app_num, install_sighandler
 from datautils import save_data, load_data, compare_data, print_stats
 from scrapeuscis import get_receipt_status
 
+CKPT_IDX = 0
+SAVE_DATA = False
+
 def main() -> None:
   """
   Parses command-line arguments, fetches the USCIS data, and prints the
   aggregate statistics.
   """
-  global WAIT_TIME, START, END, RESULTS
+  global WAIT_TIME, START, END, RESULTS, CKPT_IDX, SAVE_DATA
 
   parser = ArgumentParser(description="Get USCIS data")
   parser.add_argument('--start-range', default=197000, type=int,
@@ -32,27 +35,30 @@ def main() -> None:
   install_sighandler(exit_gracefully)
   START = args.start_range
   END = args.start_range + args.num_elts
+  SAVE_DATA = args.save_data
   if args.load_data:
     (START, END, RESULTS) = load_data(args.load_data)
   elif args.compare_data:
     compare_data(args.compare_data)
     return
   else:
+    CKPT_IDX = START
     for i in tqdm(range(START, END)):
       RESULTS.append(get_receipt_status(construct_app_num(i)))
+      CKPT_IDX += 1
       sleep(WAIT_TIME)
-  print_stats(START, END, args.save_data, array(RESULTS))
+  print_stats(START, END, SAVE_DATA, array(RESULTS))
 
 def exit_gracefully(signum, frame) -> None:
   """
   Signal handler for SIGINT (^C); prints the statistics so far and kills
   the program.
-
-  TODO: Check if user had asked us to save data, and then save the partial
-  data to a CSV file.
   """
+  global START, END, RESULTS, CKPT_IDX, SAVE_DATA
   if START != None and END != None and len(RESULTS) > 0:
-    print_stats(START, END, False, array(RESULTS))
+    # This gives us a minimum set of recoverable data
+    CKPT_IDX = START + min(len(RESULTS), CKPT_IDX - START)
+    print_stats(START, CKPT_IDX, SAVE_DATA, array(RESULTS))
   exit(0)
 
 if __name__ == "__main__":
